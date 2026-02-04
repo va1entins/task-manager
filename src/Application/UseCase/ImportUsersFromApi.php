a8c13e4bc14e2eb5a51f37399ef7381cee1b32fd
Valentyn <va1entins@yahoo.com>
2026-02-04 17:53:01 +0100

    feat(application): add import users from api use case

---
+++ src/Application/UseCase/ImportUsersFromApi.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Application\UseCase;
+
+use App\Application\Dto\ImportedUserDto;
+use App\Domain\Repository\UserRepositoryInterface;
+use App\Domain\User\User;
+use App\Domain\User\UserId;
+use App\Infrastructure\Http\JsonPlaceholder\JsonPlaceholderUserClient;
+
+final class ImportUsersFromApi
+{
+    public function __construct(
+        private JsonPlaceholderUserClient $client,
+        private UserRepositoryInterface $users
+    ) {}
+
+    public function execute(): int
+    {
+        $imported = 0;
+
+        $apiUsers = $this->client->fetchUsers();
+
+        foreach ($apiUsers as $apiUser) {
+            $dto = new ImportedUserDto(
+                id: $apiUser->id,
+                name: $apiUser->name,
+                email: $apiUser->email
+            );
+
+            $user = new User(
+                new UserId($dto->id),
+                $dto->name,
+                $dto->email
+            );
+
+            $this->users->save($user);
+            $imported++;
+        }
+
+        return $imported;
+    }
+}
