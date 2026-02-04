24b99eb4b8caf7a497cb8ae7a69acb75f2ece6cc
Valentyn <va1entins@yahoo.com>
2026-02-04 17:06:24 +0100

    feat(domain): add user aggregate

--- src/Domain/Repository/UserRepositoryInterface.php
+++ src/Domain/Repository/UserRepositoryInterface.php
@@ -3,7 +3,16 @@
 namespace App\Domain\Repository;
 
 use App\Domain\User\User;
+use App\Domain\User\UserId;
 
 interface UserRepositoryInterface
 {
+    public function save(User $user): void;
+
+    public function findById(UserId $id): ?User;
+
+    /**
+     * @return User[]
+     */
+    public function findAll(): array;
 }
--- src/Domain/User/User.php
+++ src/Domain/User/User.php
@@ -4,4 +4,24 @@ namespace App\Domain\User;
 
 final class User
 {
+    public function __construct(
+        private UserId $id,
+        private string $name,
+        private string $email
+    ) {}
+
+    public function id(): UserId
+    {
+        return $this->id;
+    }
+
+    public function name(): string
+    {
+        return $this->name;
+    }
+
+    public function email(): string
+    {
+        return $this->email;
+    }
 }
